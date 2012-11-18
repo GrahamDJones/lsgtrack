974e59504051c2d4ecc467306eda0c7d73b30cd7
Graham Jones <graham.jones@gmail.com>
2012-11-18 11:20:06 -0500

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lsgtrack::Application.config.secret_token = 'a4407953a6c4e1c6a1b7404e4ead8956281b21de0beabf48156de83c9c8fe0f18bb1a43149d2baf873b1d4c35c4300c820e0ffcfc3cf0cbfafb43b899ae5276e'
