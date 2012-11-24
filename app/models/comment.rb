1bcc672c053dba0dc9398cf12d38e75dfc910242
Graham Jones <graham.jones@gmail.com>
2012-11-23 21:22:02 -0500

    Add state to ticket and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,15 @@
 class Comment < ActiveRecord::Base
+  after_create :set_ticket_state
   belongs_to :ticket
+  delegate :project, to: :ticket
   belongs_to :user
-  attr_accessible :text, :user, :ticket
+  belongs_to :state
+  attr_accessible :text, :user, :ticket, :state_id
   validates :text, presence: true
 
+  private
+  def set_ticket_state
+    self.ticket.state = self.state
+    self.ticket.save!
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,15 @@
 admin_user = User.create(email: "admin@example.com", password: "password")
 admin_user.admin = true
 admin_user.confirm!
+State.create(:name => "New",
+             :background => "#85FF00",
+             :color => "white")
+State.create(:name => "Open",
+             :background => "#00CFFD",
+             :color => "white")
+State.create(:name => "Closed",
+             :background => "black",
+             :color => "white")
 
 unless Rails.env.production?
   Project.create(name: "Time Tracker")
