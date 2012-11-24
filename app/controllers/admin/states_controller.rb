e4a20423fc103f6c08a5770ca6a5f3c82019cb70
Graham Jones <graham.jones@gmail.com>
2012-11-24 12:22:31 -0500

    Add states controller in admin for managing states

---
+++ app/controllers/admin/states_controller.rb
@@ -0,0 +1,16 @@
+class Admin::StatesController < Admin::BaseController
+
+  def new
+    @state = State.new
+  end
+
+  def create
+    @state = State.new(params[:state])
+    if @state.save
+      redirect_to admin_states_path, notice: "State has been created."
+    else
+      flash[:alert] = "State has not been created."
+      render action: "new"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Lsgtrack::Application.routes.draw do
     resources :users do
       resources :permissions
     end
+    resources :states
   end
 
   put '/admin/users/:user_id/permissions', :to => "admin/permissions#update", :as => :update_user_permissions
