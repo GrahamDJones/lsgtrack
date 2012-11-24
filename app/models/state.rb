522035137716aa2ffefd6f076e1baf6f9aa171fd
Graham Jones <graham.jones@gmail.com>
2012-11-24 13:08:26 -0500

    Admins can now set a default state for tickets

--- app/models/state.rb
+++ app/models/state.rb
@@ -4,4 +4,20 @@ class State < ActiveRecord::Base
   def to_s
     name
   end
+
+  def default!
+    unset_current_default!
+    self.default = true
+    self.save!
+  end
+
+  private
+
+  def unset_current_default!
+    current_default_state = State.find_by_default(true)
+    if current_default_state
+      current_default_state.default = false
+      current_default_state.save!
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
 admin_user = User.create(email: "admin@example.com", password: "password")
 admin_user.admin = true
 admin_user.confirm!
-State.create(:name => "New",
-             :background => "#85FF00",
-             :color => "white")
-State.create(:name => "Open",
-             :background => "#00CFFD",
-             :color => "white")
-State.create(:name => "Closed",
-             :background => "black",
-             :color => "white")
+default_state = State.create(name: "New",
+                             background: "#85FF00",
+                             color: "white")
+default_state.default = true
+default_state.save!
+State.create(name: "Open",
+             background: "#00CFFD",
+             color: "white")
+State.create(name: "Closed",
+             background: "black",
+             color: "white")
 
 unless Rails.env.production?
   Project.create(name: "Time Tracker")
